dfe9576be93c42da1f2433163de1a355f9095fe5
maruthilakshmi18 <57175666+maruthilakshmi18@users.noreply.github.com>
2022-07-15 00:45:33 -0700

    selenium project
    Validate all

---
+++ src/test/java/nodeexecution/HRMlogintest.java
@@ -0,0 +1,20 @@
+package nodeexecution;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class HRMlogintest extends Baseclass  {
+@Parameters("OS")
+	@Test()
+	public void login () {		
+		
+    		driver.get("https://opensource-demo.orangehrmlive.com/");
+			driver.findElement(By.xpath("//input[@name='txtUsername']")).sendKeys("Admin");
+
+			driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+
+			driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
+		}
+	
+}
